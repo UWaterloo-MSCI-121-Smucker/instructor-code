c0e5b7fd7908d4fe034283fc90ea83249e77d5f0
Mark D. Smucker <profsmucker@users.noreply.github.com>
2025-10-29 14:03:53 +0000

    bug fix in print and new create

--- matrix.py
+++ matrix.py
@@ -1,3 +1,10 @@
+def create_matrix(num_rows, num_cols):
+    rows = [None] * num_rows
+    for i in range(num_rows):
+        rows[i] = [0.0] * num_cols
+    return rows
+
+
 def build_identity_matrix(n):
     """Returns an identity matrix as a list of lists (float) of size n."""
     matrix = list()
@@ -15,10 +22,14 @@ def build_identity_matrix(n):
 def print_matrix( matrix ):
     """Prints out a matrix that is represented as a list of lists (float)."""
     for i in range(len(matrix)):
-        for j in range(len(matrix)):
+        for j in range(len(matrix[i])):
             print(matrix[i][j],end="")
-            if j != len(matrix)-1:
+            if j != len(matrix[i])-1:
                 print(" ",end="")
         print()
 
 print_matrix(build_identity_matrix(3))
+print()
+
+my_matrix = create_matrix(num_rows=2,num_cols=3)
+print_matrix(my_matrix)
